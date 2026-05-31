8c47be8ac686660cceb6e9aef97fe9d529d95a60
Seu Nome <seuemail@exemplo.com>
2026-05-31 11:08:11 -0300

    fix: ajuste na libs e compillador antes estava ocasionando bugs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    // Explicitly declare Kotlin plugin versions here so Gradle can check
+    // compatibility when the plugin is also placed on the classpath by other
+    // build files or by the IDE. Versions are taken from the version catalog.
+    plugins {
+        id("org.jetbrains.kotlin.android") version "2.2.10"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
+    }
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
